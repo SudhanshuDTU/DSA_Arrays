fc498023dfd705116b5c48140a90a2b91237676e
Sudhanshu Jha <Sudhanshujha2717@gmail.com>
2023-08-03 21:17:20 +0530

    SheetQ1

---
+++ sheetQ1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    int search(int arr[], int N, int X)
+    {
+        
+       for(int i=0;i<N;i++){
+           if(arr[i]==X){
+               return i;
+           }
+       }
+       return -1;
+        
+    }
+
+};
