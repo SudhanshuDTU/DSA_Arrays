fda8a8f212b04f70dfcab063667c324cf4affa41
Sudhanshu Jha <Sudhanshujha2717@gmail.com>
2023-08-03 22:18:55 +0530

    sheetQ4

---
+++ sheetQ4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void rotate(int arr[], int n)
+{
+    int temp[n];
+    temp[0] = arr[n-1];
+    
+    for(int i=0;i<n-1;i++){
+        temp[i+1] = arr[i];
+    }
+   
+   for(int i=0;i<n;i++){
+       arr[i] = temp[i];
+   }
+}
